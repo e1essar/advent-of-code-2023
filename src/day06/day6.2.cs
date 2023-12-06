34298e9372e7504a8b1af492b09e353951be048e
e1essar <80064778+e1essar@users.noreply.github.com>
2023-12-06 17:51:52 +0700

    add: day06

---
+++ src/day06/day6.2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllText("day06.txt");
+
+            var strings = input
+                .Trim()
+                .Split("\n")
+                .Select(s => s.TrimEnd())
+                .ToArray();
+
+            var time = long.Parse(strings[0]
+                .Split(':')[1]
+                .Replace(" ", ""));
+
+            var distance = long.Parse(strings[1]
+                .Split(':')[1]
+                .Replace(" ", ""));
+
+            long result = 0;
+
+            for (long holdTime = 1; holdTime < time; holdTime++)
+                if ((time - holdTime) * holdTime > distance)
+                    result++;
+
+            Console.WriteLine(result);
+        }
+    }
+}
