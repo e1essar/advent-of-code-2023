0c1eb75885827db6e00a907d2a6f192e42e9c959
e1essar <80064778+e1essar@users.noreply.github.com>
2023-12-10 20:45:50 +0700

    upd: day06

--- src/day06/day6.2.cs
+++ src/day06/day6.2.cs
@@ -1,27 +1,12 @@
 using System;
 
-namespace Day06
-{
-    class Program
-    {
-        static void Main()
-        {
+namespace Day06 {
+    class Program {
+        static void Main() {
             var input = File.ReadAllText("day06.txt");
-
-            var strings = input
-                .Trim()
-                .Split("\n")
-                .Select(s => s.TrimEnd())
-                .ToArray();
-
-            var time = long.Parse(strings[0]
-                .Split(':')[1]
-                .Replace(" ", ""));
-
-            var distance = long.Parse(strings[1]
-                .Split(':')[1]
-                .Replace(" ", ""));
-
+            var strings = input.Trim().Split("\n").Select(s => s.TrimEnd()).ToArray();
+            var time = long.Parse(strings[0].Split(':')[1].Replace(" ", ""));
+            var distance = long.Parse(strings[1].Split(':')[1].Replace(" ", ""));
             long result = 0;
 
             for (long holdTime = 1; holdTime < time; holdTime++)
