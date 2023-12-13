029e8e7b2b3f9c4c6327f1631bb0120924dded49
e1essar <80064778+e1essar@users.noreply.github.com>
2023-12-13 13:07:36 -0500

    add: day13

---
+++ src/day13/day13_1.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class day13_1 {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("day13.txt"));
+
+    }
+}
